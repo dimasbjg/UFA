619b741f3585114a21972a5830f4a88526bd6ef8
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2022-05-22 19:28:53 +0700

    add Layout for Jadwal

---
+++ app/src/main/java/com/dimdimbjg/ufa/ui/jadwal/JadwalActivity.kt
@@ -0,0 +1,12 @@
+package com.dimdimbjg.ufa.ui.jadwal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dimdimbjg.ufa.R
+
+class JadwalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal)
+    }
+}
