b6a77513590d04483ebbd434e0697b97f06747ca
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2022-06-01 14:51:24 +0700

    create dialog for verification profile

--- app/src/main/java/com/dimdimbjg/ufa/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/dimdimbjg/ufa/ui/profile/ProfileActivity.kt
@@ -1,12 +1,15 @@
 package com.dimdimbjg.ufa.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.dimdimbjg.ufa.R
 import com.dimdimbjg.ufa.databinding.ActivityProfileBinding
+import com.dimdimbjg.ufa.ui.formubah.FormPerubahanActivity
 import com.dimdimbjg.ufa.vo.Status
 
 class ProfileActivity : AppCompatActivity() {
@@ -53,7 +56,8 @@ class ProfileActivity : AppCompatActivity() {
                     if (result.data != null) {
                         binding.textNamaLengkap.text = result.data.nama
                         binding.textNikField.text = result.data.nik.toString()
-                        binding.textJenisKelaminField.text = if (result.data.kelamin) "Laki - Laki" else "Perempuan"
+                        binding.textJenisKelaminField.text =
+                            if (result.data.kelamin) "Laki - Laki" else "Perempuan"
                         binding.textKewarganegaraanField.text = result.data.kewarganegaraan
                         binding.textTanggalLahirField.text = result.data.tanggallahir.toString()
                         binding.textStatusMenikahField.text =
@@ -61,7 +65,9 @@ class ProfileActivity : AppCompatActivity() {
                         binding.textTempatLahirField.text = result.data.tempatlahir
                         binding.textAlamatField.text = result.data.alamat
                         binding.textOrangTuaField.text =
-                            resources.getString(R.string.nama_orang_tua, result.data.orangtua1, result.data.orangtua2)
+                            resources.getString(R.string.nama_orang_tua,
+                                result.data.orangtua1,
+                                result.data.orangtua2)
                         View.VISIBLE.let {
                             binding.textNamaLengkap.visibility = it
                             binding.textNikField.visibility = it
@@ -90,5 +96,27 @@ class ProfileActivity : AppCompatActivity() {
                 }
             }
         }
+
+        binding.btVerifikasi.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Verifikasi kebenaran data")
+                .setMessage("Apakah anda sudah yakin data yang tertera sudah benar dengan data sesungguhnya dan menyetujui verifikasi data dilakukan oleh sistem?")
+                .setPositiveButton("Iya") { dialog, id ->
+                    // yes todo
+                    dialog.cancel()
+                }
+                .setNegativeButton("Tidak") { dialog, _ ->
+                    dialog.cancel()
+                }
+
+            val alert: AlertDialog = builder.create()
+            alert.show()
+        }
+
+        binding.btAjaukanPerubahan.setOnClickListener {
+            val intent = Intent(this, FormPerubahanActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
