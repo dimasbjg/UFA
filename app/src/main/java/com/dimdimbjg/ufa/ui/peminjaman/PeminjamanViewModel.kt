42fccecb08a59c702a3aa7ddf0d6fa86dde03439
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2022-06-07 12:32:49 +0700

    add viewmodel for peminjaman (empty sekeleton)

---
+++ app/src/main/java/com/dimdimbjg/ufa/ui/peminjaman/PeminjamanViewModel.kt
@@ -0,0 +1,10 @@
+package com.dimdimbjg.ufa.ui.peminjaman
+
+import androidx.lifecycle.ViewModel
+import com.dimdimbjg.ufa.data.Repository
+
+class PeminjamanViewModel : ViewModel() {
+
+    val repository = Repository()
+
+}
