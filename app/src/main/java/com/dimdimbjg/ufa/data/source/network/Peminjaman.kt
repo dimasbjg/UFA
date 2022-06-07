67fc54a9c8d5c7e9382e353486e181016eda5856
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2022-06-07 12:31:49 +0700

    add peminjaman data class to capture response from backend

---
+++ app/src/main/java/com/dimdimbjg/ufa/data/source/network/Peminjaman.kt
@@ -0,0 +1,9 @@
+package com.dimdimbjg.ufa.data.source.network
+
+data class Peminjaman(
+	val barang: String = "",
+	val jumlah: Int = 0,
+	val untuk: String = "",
+	val verified: Boolean = false
+)
+
